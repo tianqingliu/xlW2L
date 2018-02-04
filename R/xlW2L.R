f063cefd9e0399be7e94bd372bf08c871f3daef8
tianqingliu <31743030+tianqingliu@users.noreply.github.com>
2018-02-03 22:38:41 -0500

    v2.0

--- R/xlW2L.R
+++ R/xlW2L.R
@@ -1,20 +1,35 @@
 #' Convert Execl Wide Table to Long Data Frame
 #'
 #' This function converts Execl Wide Table to Long Data Frame.
-#' @param filePath the Excel file with extension .xlsx, .xls, xlsm.
+#' @param filePath the Excel file with extension .xlsx, .xls, .xlsm, and .csv.  If csv file is selected, other arguments are not needed.
 #' @param shtNum an Excel sheet number for data table, defaults to 1.
 #' @param celIStart an Excel data starting Cell Index (upper left corner), e.g., em218, eM218, EM218, or 218Em, 218em (with max.2-letter column header).
 #' @param cellEnd an Excel data ending Cell Index (bottom right corner), e.g., dW543 (with max.2-letter column header).
 #' @param x1cell an Excel Cell Index for cell with key header, which separates identification variables from Y-value varables, e.g., bf38 (with max.2-letter column header).
 #' @param x1Name optional key header, defaults to "" will use the original Excel key name if it exists.
 #' @param yName optional response variable name for y-values returned in Long Data Frame, defaults to "" will use "Y" for header of y-value column in Long Data Frame.
-#' @return xlW2L(..)$x1T or xlW2L(..)[1] -- the original Excel table;  xlW2L(..)wideF or xlW2L(..)[2] -- the wide form after cleaning up;  xlW2L(..)longF or xlW2L(..)[3] -- the long data form.
+#' @return xlW2L(..)$x1T or xlW2L(..)[1] -- the original Excel table;  xlW2L(..)wideF or xlW2L(..)[2] -- the wide form after cleaning up;  xlW2L(..)longF or xlW2L(..)[3] -- the long data form. If the original file is .csv type, all three objects returned are the original csv table.
 #' @keywords Excel
 #' @export
 #' @examples
 #' xlW2L(filePath,,"e8","o44","i8")$longF
+#' xlW2L(filePath)$longF -- if original file is .cvs type, it returns the original cvs table.
+
 
 xlW2L <- function(filePath, shtNum = 1, celIStart, cellEnd, x1cell, x1Name = "", yName = "") {
+  
+  if (tools::file_ext(filePath) == "csv") {
+    daf <- read.csv(filePath)
+    xsnum <- ncol(daf)
+    for (i in 1:xsnum) {
+      if (inherits(daf[, i], "Date")) 
+        daf[, i] <- format(daf[, i], "%D")  #if a column is Date, change to desired date format
+    }
+    W2L <- list(xlT = daf, wideF = daf, longF = daf)
+    return(W2L)  #return(W2L) can be also turned off, -if it is on, both objects will be printed out when function is called.
+    # The objects can be called with: W2L(...)$xlT or W2L(...)$wideF
+  }
+  
   if (yName == "") 
     yName <- "Y"
   
@@ -58,8 +73,8 @@ xlW2L <- function(filePath, shtNum = 1, celIStart, cellEnd, x1cell, x1Name = "",
       ifelse(!is.na(wideF[1, i]) && names(wideF)[i] != "NA", a <- 0, 
         b <- c(b, i))  #determine col#s of xVariables with first cell NA
       # must use !is.na(wideF[1,i]), because 'Error in if (is.na(wideF[1,
-      # i])) { : argument is of length zero'
-      ## NA for header is treated as charaters in is.na(names(wideF)[i])
+      # i])) { : argument is of length zero' NA for header is treated as
+      # charaters in is.na(names(wideF)[i])
     }
     ncd <- length(b)  #numbers of columns deleted of ID columns
     if (ncd > 0) 
@@ -79,15 +94,12 @@ xlW2L <- function(filePath, shtNum = 1, celIStart, cellEnd, x1cell, x1Name = "",
     vari <- 1:(ce - cs + 1 - ncd - xcd)
     if (xsnum == ncd && xsnum != 0) 
       vari <- 1:(ce - cs - ncd - xcd)
-    longF <- reshape(wideD, 
-      v.names = yName, 
-      varying = vari, 
-      direction = "long", 
-      new.row.names = 1:(nrow(wideD) * length(vari)), 
-      times = names(wideD)[vari])  #to preserve original 'numeric' column names in long column
+    longF <- reshape(wideD, v.names = yName, varying = vari, direction = "long", 
+      new.row.names = 1:(nrow(wideD) * length(vari)), times = names(wideD)[vari])  #to preserve original 'numeric' column names in long column
     # timevar=names(wideD)[xsnum+1-ncd], idvar=idvari,
-    # drop=names(wideD)[xsnum+1-ncd], #drop colX1 column, ncd -- adjust for col deleted 
-    ##new.row.names = 1:(nrow(wideD)*length(vari)), #1: new long row numbers
+    # drop=names(wideD)[xsnum+1-ncd], #drop colX1 column, ncd -- adjust for
+    # col deleted new.row.names = 1:(nrow(wideD)*length(vari)), #1: new
+    # long row numbers
     
     longF$id <- NULL  #delete id column
     names(longF)[1] <- "X"
@@ -95,17 +107,13 @@ xlW2L <- function(filePath, shtNum = 1, celIStart, cellEnd, x1cell, x1Name = "",
     # having ID columns
     vari <- (xsnum + 2 - ncd):(ce - cs + 1 - ncd - xcd)  # column list for X1 values (and y-values below )
     idvari <- names(wideD)[1:(xsnum - ncd)]
-    longF <- reshape(wideD, 
-      timevar = names(wideD)[xsnum + 1 - ncd], 
-      v.names = yName, 
-      varying = vari, 
-      idvar = idvari, 
-      drop = names(wideD)[xsnum + 1 - ncd], 
-      direction = "long", 
-      new.row.names = 1:(nrow(wideD) * length(vari)), 
-      times = names(wideD)[vari])  #to preserve original 'numeric' column names in long column
-    ## drop=names(wideD)[xsnum+1-ncd], #drop colX1 column, ncd -- adjust for col deleted 
-    ## new.row.names = 1:(nrow(wideD)*length(vari)), #1: new long row numbers
+    longF <- reshape(wideD, timevar = names(wideD)[xsnum + 1 - ncd], 
+      v.names = yName, varying = vari, idvar = idvari, drop = names(wideD)[xsnum + 
+        1 - ncd], direction = "long", new.row.names = 1:(nrow(wideD) * 
+        length(vari)), times = names(wideD)[vari])  #to preserve original 'numeric' column names in long column
+    ## drop=names(wideD)[xsnum+1-ncd], #drop colX1 column, ncd -- adjust for
+    ## col deleted new.row.names = 1:(nrow(wideD)*length(vari)), #1: new
+    ## long row numbers
   }  #close if
   
   longF <- na.omit(longF)  #remove any row with blank cells
@@ -119,4 +127,4 @@ xlW2L <- function(filePath, shtNum = 1, celIStart, cellEnd, x1cell, x1Name = "",
   W2L <- list(xlT = daf, wideF = wideF, longF = longF)
   return(W2L)  #return(W2L) can be also turned off, -if it is on, both objects will be printed out when function is called.
   # The objects can be called with: W2L(...)$xlT or W2L(...)$wideF
-}  #close xlW2L function
+}  #close xlW2L function########### 
